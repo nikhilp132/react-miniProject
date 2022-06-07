c8c26a520117736d117ffac4e4052ecb72c5de07
nikhilp132 <nikhilparadkar200@gmail.com>
2022-06-07 12:27:38 +0530

    Initial commit

---
+++ src/Card.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Card(props) {
+  return (
+    <div className="cards">
+      <div className="card">
+        <img src={props.posterSrc} alt="poster" className="card_img" />
+        <div className="card_info">
+          <span className="card_category">Netflix originals</span>
+          <h3 className="card_title">{props.title}</h3>
+          <a href={props.link} target="_blank">
+            <button> Watch Now</button>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
