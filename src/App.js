23d9cb7cceeb657d692b994c9036e05a334e4cf5
nikhilp132 <nikhilparadkar200@gmail.com>
2022-06-07 12:49:27 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -5,21 +5,24 @@ import './style.css';
 function App() {
   return (
     <>
-      <Card
-        posterSrc="https://terrigen-cdn-dev.marvel.com/content/prod/1x/punisher_s2_vertical-main_rgb.jpg"
-        title="The Punisher"
-        link="https://www.netflix.com/title/80117498"
-      />
-      <Card
-        posterSrc="https://m.media-amazon.com/images/M/MV5BM2ZmYjUxZDYtMmZlMi00YjlkLTkwNTItMjcxZDRlOTI3ZTNmXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg"
-        title="The Spy"
-        link="https://www.netflix.com/in/title/80178151"
-      />
-      <Card
-        posterSrc="https://m.media-amazon.com/images/M/MV5BNzVkYWY4NzYtMWFlZi00YzkwLThhZDItZjcxYTU4ZTMzMDZmXkEyXkFqcGdeQXVyODUxOTU0OTg@._V1_.jpg"
-        title="Brooklyn 99"
-        link="https://www.netflix.com/in/title/70281562#:~:text=2013%20%7C%20U%2FA%2016%2B,Samberg%2CAndre%20Braugher%2CStephanie%20Beatriz"
-      />
+      <h1 className="heading">List of top Netflix Series</h1>
+      <div className="CardContainer">
+        <Card
+          posterSrc="https://terrigen-cdn-dev.marvel.com/content/prod/1x/punisher_s2_vertical-main_rgb.jpg"
+          title="The Punisher"
+          link="https://www.netflix.com/title/80117498"
+        />
+        <Card
+          posterSrc="https://m.media-amazon.com/images/M/MV5BM2ZmYjUxZDYtMmZlMi00YjlkLTkwNTItMjcxZDRlOTI3ZTNmXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg"
+          title="The Spy"
+          link="https://www.netflix.com/in/title/80178151"
+        />
+        <Card
+          posterSrc="https://m.media-amazon.com/images/M/MV5BNzVkYWY4NzYtMWFlZi00YzkwLThhZDItZjcxYTU4ZTMzMDZmXkEyXkFqcGdeQXVyODUxOTU0OTg@._V1_.jpg"
+          title="Brooklyn 99"
+          link="https://www.netflix.com/in/title/70281562#:~:text=2013%20%7C%20U%2FA%2016%2B,Samberg%2CAndre%20Braugher%2CStephanie%20Beatriz"
+        />
+      </div>
     </>
   );
 }
