c5ffed5152d964ceb804556b30ad90c566b7c415
nikhilp132 <nikhilparadkar200@gmail.com>
2022-06-08 12:14:05 +0530

    1 file added

---
+++ src/Data.jsx
@@ -0,0 +1,8 @@
+const Data = [{
+  posterImg:
+  'https://terrigen-cdn-dev.marvel.com/content/prod/1x/punisher_s2_vertical-main_rgb.jpg',
+sTitle: 'The Punisher',
+sLink: 'https://www.netflix.com/title/80117498'
+}];
+
+export default Data;
