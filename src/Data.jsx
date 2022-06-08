91a3462eca133be57dc9d27edb8747256ad366ec
nikhilp132 <nikhilparadkar200@gmail.com>
2022-06-08 12:14:26 +0530

    1 file modified

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,8 +1,23 @@
-const Data = [{
-  posterImg:
-  'https://terrigen-cdn-dev.marvel.com/content/prod/1x/punisher_s2_vertical-main_rgb.jpg',
-sTitle: 'The Punisher',
-sLink: 'https://www.netflix.com/title/80117498'
-}];
+const Data = [
+  {
+    posterImg:
+      'https://terrigen-cdn-dev.marvel.com/content/prod/1x/punisher_s2_vertical-main_rgb.jpg',
+    sTitle: 'The Punisher',
+    sLink: 'https://www.netflix.com/title/80117498',
+  },
+  {
+    posterImg:
+      'https://m.media-amazon.com/images/M/MV5BM2ZmYjUxZDYtMmZlMi00YjlkLTkwNTItMjcxZDRlOTI3ZTNmXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg',
+    sTitle: 'The Spy',
+    sLink: 'https://www.netflix.com/in/title/80178151',
+  },
+  {
+    posterImg:
+      'https://m.media-amazon.com/images/M/MV5BNzVkYWY4NzYtMWFlZi00YzkwLThhZDItZjcxYTU4ZTMzMDZmXkEyXkFqcGdeQXVyODUxOTU0OTg@._V1_.jpg',
+    sTitle: 'Brooklyn 99',
+    sLink:
+      'https://www.netflix.com/in/title/70281562#:~:text=2013%20%7C%20U%2FA%2016%2B,Samberg%2CAndre%20Braugher%2CStephanie%20Beatriz',
+  },
+];
 
 export default Data;
